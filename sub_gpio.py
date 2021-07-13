cac5e0af276856966396ffcd23a9df7dc5232c89
NonThanadol <81633477+nonthanadol@users.noreply.github.com>
2021-07-13 03:52:02 +0000

    Update sub_gpio.py

--- sub_gpio.py
+++ sub_gpio.py
@@ -5,12 +5,14 @@ from std_msgs.msg import Bool
 import RPi.GPIO as GPIO
 
 LED_GPIO = 20
+GPIO.setwarnings(False)
 
 def button_state_callback(msg):
     GPIO.output(LED_GPIO, msg.data)
 
 if __name__ == '__main__':
     rospy.init_node('led_actuator')
+    print('Start!!!')
 
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(LED_GPIO, GPIO.OUT)
